278f283f6230f92f661f32f2dd165e42e3e18e57
Mollie <molliewatkins388@gmail.com>
2025-11-27 11:17:18 -0500

    Initial Commit

---
+++ CIS1202_Assignment15a/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CIS1202_Assignment15a
+//
+//  Created by Mollie Watkins on 11/27/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
