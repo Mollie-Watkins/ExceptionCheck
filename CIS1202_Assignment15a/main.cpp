6d99c9d4680b8c79f55c2fadbd17f04e77ff8f14
Mollie <molliewatkins388@gmail.com>
2025-11-28 16:33:48 -0500

    Comment additions and a few minor code improvements

--- CIS1202_Assignment15a/main.cpp
+++ CIS1202_Assignment15a/main.cpp
@@ -1,26 +1,28 @@
 // Mollie Watkins
 // CIS 1202 801
 // November 27, 2025
+// Driver program to test the character function
 
-#include <iostream>
 #include "Exceptions.h"
+#include <iostream>
+#include <cctype>
+
 
 using namespace std;
-
 
 char character(char, int);
 
 
-// Name:
-// Purpose:
-// Parameters:
-// Returns:
+// main
+// Purpose: Uses an array of test cases to test the character function
+// Parameters: None
+// Returns: EXIT_SUCCESS to indicate a successful run
 int main()
 {
     const int SIZE = 5;
-    char startTestArray[SIZE] = {'a', 'a', 'Z', '?', 'A'};
     int offsetTestArray[SIZE] = {1, -1, -1, 5, 32};
-    
+    char startTestArray[SIZE] = {'a', 'a', 'Z', '?', 'A'};
+
     
     for(int i = 0; i < SIZE; ++i)
     {
@@ -32,23 +34,26 @@ int main()
         }
         catch(Exceptions::invalidCharacterException)
         {
-            cout << "ERROR: invalid character." << endl;
+            cout << "ERROR: Invalid character." << endl;
         }
         catch(Exceptions::invalidRangeException)
         {
-            cout << "ERROR: invalid range." << endl;
+            cout << "ERROR: Invalid range." << endl;
         }
     }
     
     
+    cout << endl;
     return EXIT_SUCCESS;
 }
 
 
-// Name:
-// Purpose:
+// Name: character
+// Purpose: Calculates a target character from the given parameters and validates that it falls within the allowed range
 // Parameters:
-// Returns:
+//      start - the starting character used in the target calculation
+//      offset - an int used in the target calculation
+// Returns: The calculated character
 char character(char start, int offset)
 {
     char    target = start + offset,
